368105d296443b3984fa1c38d4088384b1ddea01
austinandrade <austinmandrade@gmail.com>
2021-07-09 21:51:16 -0400

    Create specialties table, model, and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_014136) do
+ActiveRecord::Schema.define(version: 2021_07_10_015040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "insurances", force: :cascade do |t|
     t.string "company"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "specialties", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
