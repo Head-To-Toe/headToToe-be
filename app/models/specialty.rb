35691a741c02b61d5d1470beca92d32d6f99d54f
austinandrade <austinmandrade@gmail.com>
2021-07-10 13:54:36 -0400

    Create specialty model and test

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,2 +1,8 @@
 class Specialty < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
+  has_many :doctor_specialties
+  has_many :mhp_specialties
+  has_many :doctors, through: :doctor_specialties
+  has_many :mental_health_professionals, through: :mhp_specialties
 end
