4d5dbb163926f46a1517fb8cfffb8a925a667ff6
austinandrade <austinmandrade@gmail.com>
2021-07-10 13:53:59 -0400

    Incorporate joins table relationships & Add vetted validation

--- spec/models/mental_health_professional_spec.rb
+++ spec/models/mental_health_professional_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe MentalHealthProfessional, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of :first_name}
+    it { should validate_presence_of :last_name}
+    it { should validate_presence_of :state}
+    it { should validate_presence_of :vetted}
+  end
+
+  describe "relationships" do
+    it { should have_many(:mhp_specialties) }
+    it { should have_many(:mhp_insurances) }
+    it { should have_many(:specialties).through(:mhp_specialties) }
+    it { should have_many(:insurances).through(:mhp_insurances) }
+  end
 end
