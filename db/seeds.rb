b60cfad7250005ef72bf5eadafc8c497b718454f
austinandrade <austinmandrade@gmail.com>
2021-07-09 23:23:18 -0400

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Insurance.destroy_all
+Specialty.destroy_all
+Doctor.destroy_all
+MentalHealthProfessional.destroy_all
+
+random_boolean = [true, false].sample
+
+15.times { Insurance.create!( company: Faker::Company.name ) }
+15.times { Specialty.create!( name: Faker::Games::DnD.language ) }
+
+150.times do Doctor.create!(
+    first_name: Faker::Creature::Animal.name,
+    last_name: Faker::Games::Pokemon.name,
+    state: Faker::Address.state,
+    city: Faker::Address.city,
+    zip: '19384',
+    street: Faker::Address.street_address,
+    unit: Faker::Address.secondary_address,
+    vetted: random_boolean,
+    phone: '234-456-2244',
+    specialty_id: Specialty.all.sample.id,
+    insurance_id: Insurance.all.sample.id)
+end
+
+150.times do MentalHealthProfessional.create!(
+    first_name: Faker::Creature::Animal.name,
+    last_name: Faker::Games::Pokemon.name,
+    state: Faker::Address.state,
+    city: Faker::Address.city,
+    street: Faker::Address.street_address,
+    zip: '10385',
+    unit: Faker::Address.secondary_address,
+    vetted: random_boolean,
+    phone: '234-456-2244',
+    cost: '$123-150',
+    specialty_id: Specialty.all.sample.id,
+    insurance_id: Insurance.all.sample.id)
+end
