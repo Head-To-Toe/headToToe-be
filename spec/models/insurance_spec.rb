30829686bd2aad21d8371ccd011c3f56f5aaf69b
austinandrade <austinmandrade@gmail.com>
2021-07-10 13:53:00 -0400

    Create insurance model and test

--- spec/models/insurance_spec.rb
+++ spec/models/insurance_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Insurance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of :company }
+    it { should validate_uniqueness_of :company}
+  end
+
+  describe "relationships" do
+    it { should have_many(:doctor_insurances) }
+    it { should have_many(:mhp_insurances) }
+    it { should have_many(:doctors).through(:doctor_insurances) }
+    it { should have_many(:mental_health_professionals).through(:mhp_insurances) }
+  end
 end
+
