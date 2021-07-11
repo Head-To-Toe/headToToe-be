30cf2aebddfec01415e946388d563005ce218bc8
Aidan McGuire <>
2021-07-11 15:22:29 -0600

    Add post /add_professional

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "/medical_professionals", to: 'medical_professionals#index'
+
+      post "/add_professional", to: 'add_professional#create'
     end
   end
 end
