fbda83f22a7e2c3f5d4e66ffe7a89175b4f9d87b
Aidan McGuire <>
2021-07-11 18:16:13 -0600

    Appease rubocop

--- app/controllers/api/v1/add_professional_controller.rb
+++ app/controllers/api/v1/add_professional_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AddProfessionalController < ApplicationController
   def create
     return render status: :unauthorized if unauthorized
+
     case params[:profession]
     when 'doctor'
       create_doctor
@@ -14,8 +15,8 @@ class Api::V1::AddProfessionalController < ApplicationController
   def create_doctor
     new_doctor = Doctor.new(doctor_params)
     if new_doctor.save
-      set_insurance(new_doctor)
-      set_specialties(new_doctor)
+      add_insurance(new_doctor)
+      add_specialties(new_doctor)
       render status: :created
     else
       render status: :bad_request
@@ -25,15 +26,15 @@ class Api::V1::AddProfessionalController < ApplicationController
   def create_mhp
     new_mhp = MentalHealthProfessional.new(mhp_params)
     if new_mhp.save
-      set_insurance(new_mhp)
-      set_specialties(new_mhp)
+      add_insurance(new_mhp)
+      add_specialties(new_mhp)
       render status: :created
     else
       render status: :bad_request
     end
   end
-  
-  def set_insurance(professional)
+
+  def add_insurance(professional)
     params[:insurance].each do |insurance|
       i = Insurance.find_or_create_by(company: insurance)
       case params[:profession]
@@ -44,8 +45,8 @@ class Api::V1::AddProfessionalController < ApplicationController
       end
     end
   end
-  
-  def set_specialties(professional)
+
+  def add_specialties(professional)
     params[:specialties].each do |specialty|
       i = Specialty.find_or_create_by(name: specialty)
       case params[:profession]
@@ -59,16 +60,17 @@ class Api::V1::AddProfessionalController < ApplicationController
 
   def doctor_params
     params.require(:add_professional)
-    .permit(:first_name, :last_name, :street, :unit, :city, :state, :zip, :phone)
+          .permit(:first_name, :last_name, :street, :unit, :city, :state, :zip, :phone)
   end
 
   def mhp_params
     params.require(:add_professional)
-    .permit(:first_name, :last_name, :street, :unit, :city, :state, :zip, :phone, :cost)
+          .permit(:first_name, :last_name, :street, :unit, :city, :state, :zip, :phone, :cost)
   end
 
   def unauthorized
-    return true if request.headers["api-key"] != 'aidanisthebest'
+    return true if request.headers['api-key'] != 'aidanisthebest'
+
     false
   end
 end
