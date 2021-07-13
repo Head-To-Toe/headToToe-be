d43b558550411f53358beb0ef8fb2800516f811d
austinandrade <austinmandrade@gmail.com>
2021-07-12 21:42:13 -0400

    Ignore entire spec directory in coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
 SimpleCov.start do
-  add_filter "/spec/rails_helper.rb"
+  add_filter "/spec"
 end
 
 RSpec.configure do |config|
