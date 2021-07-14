747eb68fffa87ec645379fc0609246ac4dd17a0c
austinandrade <austinmandrade@gmail.com>
2021-07-13 23:52:28 -0400

    Add destroy method to /medical_professionals endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :medical_professionals, only: [:index, :create, :update]
+      resources :medical_professionals, only: [:index, :create, :update, :destroy]
     end
   end
 end
