1a177fa101180ba0523d10f706d03435e3ccc467
austinandrade <austinmandrade@gmail.com>
2021-07-14 19:16:22 -0400

    Allow post, patch, and delete requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,7 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get]
+      methods: [:get, :post, :patch, :delete]
       # methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
