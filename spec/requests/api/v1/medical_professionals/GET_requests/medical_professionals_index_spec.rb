50f03647cbd3d223be268209a66f470db1fe7f21
Aidan McGuire <>
2021-07-13 20:29:46 -0600

    Add testing for all param combos with vetted param

--- spec/requests/api/v1/medical_professionals/GET_requests/medical_professionals_index_spec.rb
+++ spec/requests/api/v1/medical_professionals/GET_requests/medical_professionals_index_spec.rb
@@ -145,7 +145,7 @@ describe "medical professional requests" do
       end
     end
 
-    it 'does not require the state param, returns all vetted resources' do 
+    xit 'does not require the state param, returns all vetted resources' do 
       doctors               = create_list(:doctor, 20, vetted: true)
 
       insurances            = create_list(:insurance, 10)
@@ -314,6 +314,424 @@ describe "medical professional requests" do
 
         expect(unvetted).to have_key(:phone)
         expect(unvetted[:phone]).to be_a(String)
+
+        expect(unvetted).to have_key(:cost)
+        expect(unvetted[:cost]).to be_a(String)
+
+        expect(unvetted).to have_key(:vetted)
+        expect(unvetted[:vetted]).to eq(false)
+
+        expect(unvetted).to have_key(:specialties)
+        expect(unvetted[:specialties]).to be_an(Array)
+
+        expect(unvetted).to have_key(:insurances)
+        expect(unvetted[:insurances]).to be_an(Array)
+      end
+    end
+
+    it 'returns all unvetted doctors if vetted=false & type=doctor' do 
+      create(:mental_health_professional, vetted: false)
+
+      doctors            = create_list(:doctor, 20, vetted: false)
+
+      insurances         = create_list(:insurance, 10)
+      specialties        = create_list(:specialty, 10)
+
+      doctor_insurances  = create_list(:doctor_insurance, 10, doctor: doctors.sample, insurance: insurances.sample)
+      doctor_specialties = create_list(:doctor_specialty, 10, doctor: doctors.sample, specialty: specialties.sample)
+
+      get '/api/v1/medical_professionals', params: {vetted: false, type: 'doctor'}
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      unvetted_pros = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(unvetted_pros).to have_key(:data)
+      expect(unvetted_pros[:data].count).to eq(3)
+      expect(unvetted_pros[:data]).to be_a(Hash)
+
+      expect(unvetted_pros[:data]).to have_key(:id)
+      expect(unvetted_pros[:data][:id]).to eq(nil)
+      
+      expect(unvetted_pros[:data]).to have_key(:type)
+      expect(unvetted_pros[:data][:type]).to eq('unvetted_professionals')
+
+      expect(unvetted_pros[:data]).to have_key(:attributes)
+      expect(unvetted_pros[:data][:attributes]).to be_a(Hash)
+      expect(unvetted_pros[:data][:attributes].keys).to eq([:doctors, :mhps])
+
+      expect(unvetted_pros[:data][:attributes][:doctors]).to be_an(Array)
+      expect(unvetted_pros[:data][:attributes][:mhps]).to eq(nil)
+      
+      expect(unvetted_pros[:data][:attributes][:doctors].count).to eq(20)
+
+      unvetted_pros[:data][:attributes][:doctors].each do |unvetted|
+        expect(unvetted).to have_key(:id)
+        expect(unvetted[:id]).to be_an(Integer)
+
+        expect(unvetted).to have_key(:first_name)
+        expect(unvetted[:first_name]).to be_a(String)
+
+        expect(unvetted).to have_key(:state)
+        expect(unvetted[:state]).to be_a(String)
+
+        expect(unvetted).to have_key(:city)
+        expect(unvetted[:city]).to be_a(String)
+
+        expect(unvetted).to have_key(:street)
+        expect(unvetted[:street]).to be_a(String)
+
+        expect(unvetted).to have_key(:unit)
+        expect(unvetted[:unit]).to be_a(String)
+
+        expect(unvetted).to have_key(:zip)
+        expect(unvetted[:zip]).to be_a(String)
+
+        expect(unvetted).to have_key(:phone)
+        expect(unvetted[:phone]).to be_a(String)
+
+        expect(unvetted).to have_key(:vetted)
+        expect(unvetted[:vetted]).to eq(false)
+
+        expect(unvetted).to have_key(:specialties)
+        expect(unvetted[:specialties]).to be_an(Array)
+
+        expect(unvetted).to have_key(:insurances)
+        expect(unvetted[:insurances]).to be_an(Array)
+      end
+    end
+
+    it 'returns all unvetted doctors in CO if vetted=false & type=doctor & state=Colorado' do
+      create(:mental_health_professional, vetted: false)
+
+      doctors            = create_list(:doctor, 20, vetted: false)
+
+      insurances         = create_list(:insurance, 10)
+      specialties        = create_list(:specialty, 10)
+
+      doctor_insurances  = create_list(:doctor_insurance, 10, doctor: doctors.sample, insurance: insurances.sample)
+      doctor_specialties = create_list(:doctor_specialty, 10, doctor: doctors.sample, specialty: specialties.sample)
+
+      get '/api/v1/medical_professionals', params: {vetted: false, type: 'doctor', state: 'Colorado'}
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      unvetted_pros = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(unvetted_pros).to have_key(:data)
+      expect(unvetted_pros[:data].count).to eq(3)
+      expect(unvetted_pros[:data]).to be_a(Hash)
+
+      expect(unvetted_pros[:data]).to have_key(:id)
+      expect(unvetted_pros[:data][:id]).to eq(nil)
+      
+      expect(unvetted_pros[:data]).to have_key(:type)
+      expect(unvetted_pros[:data][:type]).to eq('unvetted_professionals')
+
+      expect(unvetted_pros[:data]).to have_key(:attributes)
+      expect(unvetted_pros[:data][:attributes]).to be_a(Hash)
+      expect(unvetted_pros[:data][:attributes].keys).to eq([:doctors, :mhps])
+
+      expect(unvetted_pros[:data][:attributes][:doctors]).to be_an(Array)
+      expect(unvetted_pros[:data][:attributes][:mhps]).to eq(nil)
+      
+      expect(unvetted_pros[:data][:attributes][:doctors].count.zero?).to eq(false)
+
+      unvetted_pros[:data][:attributes][:doctors].each do |unvetted|
+        expect(unvetted).to have_key(:id)
+        expect(unvetted[:id]).to be_an(Integer)
+
+        expect(unvetted).to have_key(:first_name)
+        expect(unvetted[:first_name]).to be_a(String)
+
+        expect(unvetted).to have_key(:state)
+        expect(unvetted[:state]).to eq('Colorado')
+
+        expect(unvetted).to have_key(:city)
+        expect(unvetted[:city]).to be_a(String)
+
+        expect(unvetted).to have_key(:street)
+        expect(unvetted[:street]).to be_a(String)
+
+        expect(unvetted).to have_key(:unit)
+        expect(unvetted[:unit]).to be_a(String)
+
+        expect(unvetted).to have_key(:zip)
+        expect(unvetted[:zip]).to be_a(String)
+
+        expect(unvetted).to have_key(:phone)
+        expect(unvetted[:phone]).to be_a(String)
+
+        expect(unvetted).to have_key(:vetted)
+        expect(unvetted[:vetted]).to eq(false)
+
+        expect(unvetted).to have_key(:specialties)
+        expect(unvetted[:specialties]).to be_an(Array)
+
+        expect(unvetted).to have_key(:insurances)
+        expect(unvetted[:insurances]).to be_an(Array)
+      end
+    end
+
+    it 'returns all unvetted mhps if vetted=false & type=mhp' do 
+      create(:doctor, vetted: false)
+
+      mhps            = create_list(:mental_health_professional, 20, vetted: false)
+
+      insurances      = create_list(:insurance, 10)
+      specialties     = create_list(:specialty, 10)
+
+      mhp_insurances  = create_list(:mhp_insurance, 10, mental_health_professional: mhps.sample, insurance: insurances.sample)
+      mhp_specialties = create_list(:mhp_specialty, 10, mental_health_professional: mhps.sample, specialty: specialties.sample)
+
+      get '/api/v1/medical_professionals', params: {vetted: false, type: 'mhp'}
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      unvetted_pros = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(unvetted_pros).to have_key(:data)
+      expect(unvetted_pros[:data].count).to eq(3)
+      expect(unvetted_pros[:data]).to be_a(Hash)
+
+      expect(unvetted_pros[:data]).to have_key(:id)
+      expect(unvetted_pros[:data][:id]).to eq(nil)
+      
+      expect(unvetted_pros[:data]).to have_key(:type)
+      expect(unvetted_pros[:data][:type]).to eq('unvetted_professionals')
+
+      expect(unvetted_pros[:data]).to have_key(:attributes)
+      expect(unvetted_pros[:data][:attributes]).to be_a(Hash)
+      expect(unvetted_pros[:data][:attributes].keys).to eq([:doctors, :mhps])
+
+      expect(unvetted_pros[:data][:attributes][:doctors]).to eq(nil)
+      expect(unvetted_pros[:data][:attributes][:mhps]).to be_an(Array)
+      
+      expect(unvetted_pros[:data][:attributes][:mhps].count).to eq(20)
+
+      unvetted_pros[:data][:attributes][:mhps].each do |unvetted|
+        expect(unvetted).to have_key(:id)
+        expect(unvetted[:id]).to be_an(Integer)
+
+        expect(unvetted).to have_key(:first_name)
+        expect(unvetted[:first_name]).to be_a(String)
+
+        expect(unvetted).to have_key(:state)
+        expect(unvetted[:state]).to be_a(String)
+
+        expect(unvetted).to have_key(:city)
+        expect(unvetted[:city]).to be_a(String)
+
+        expect(unvetted).to have_key(:street)
+        expect(unvetted[:street]).to be_a(String)
+
+        expect(unvetted).to have_key(:unit)
+        expect(unvetted[:unit]).to be_a(String)
+
+        expect(unvetted).to have_key(:zip)
+        expect(unvetted[:zip]).to be_a(String)
+
+        expect(unvetted).to have_key(:phone)
+        expect(unvetted[:phone]).to be_a(String)
+
+        expect(unvetted).to have_key(:cost)
+        expect(unvetted[:cost]).to be_a(String)
+
+        expect(unvetted).to have_key(:vetted)
+        expect(unvetted[:vetted]).to eq(false)
+
+        expect(unvetted).to have_key(:specialties)
+        expect(unvetted[:specialties]).to be_an(Array)
+
+        expect(unvetted).to have_key(:insurances)
+        expect(unvetted[:insurances]).to be_an(Array)
+      end
+    end
+
+    it 'returns all unvetted mhps in CO if vetted=false & type=mhp & state=Colorado' do 
+      create(:doctor, vetted: false)
+
+      mhps            = create_list(:mental_health_professional, 20, vetted: false)
+
+      insurances      = create_list(:insurance, 10)
+      specialties     = create_list(:specialty, 10)
+
+      mhp_insurances  = create_list(:mhp_insurance, 10, mental_health_professional: mhps.sample, insurance: insurances.sample)
+      mhp_specialties = create_list(:mhp_specialty, 10, mental_health_professional: mhps.sample, specialty: specialties.sample)
+
+      get '/api/v1/medical_professionals', params: {vetted: false, type: 'mhp', state: 'Colorado'}
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      unvetted_pros = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(unvetted_pros).to have_key(:data)
+      expect(unvetted_pros[:data].count).to eq(3)
+      expect(unvetted_pros[:data]).to be_a(Hash)
+
+      expect(unvetted_pros[:data]).to have_key(:id)
+      expect(unvetted_pros[:data][:id]).to eq(nil)
+      
+      expect(unvetted_pros[:data]).to have_key(:type)
+      expect(unvetted_pros[:data][:type]).to eq('unvetted_professionals')
+
+      expect(unvetted_pros[:data]).to have_key(:attributes)
+      expect(unvetted_pros[:data][:attributes]).to be_a(Hash)
+      expect(unvetted_pros[:data][:attributes].keys).to eq([:doctors, :mhps])
+      
+      expect(unvetted_pros[:data][:attributes][:doctors]).to eq(nil)
+      expect(unvetted_pros[:data][:attributes][:mhps]).to be_an(Array)
+      
+      expect(unvetted_pros[:data][:attributes][:mhps].count.zero?).to eq(false)
+
+      unvetted_pros[:data][:attributes][:mhps].each do |unvetted|
+        expect(unvetted).to have_key(:id)
+        expect(unvetted[:id]).to be_an(Integer)
+
+        expect(unvetted).to have_key(:first_name)
+        expect(unvetted[:first_name]).to be_a(String)
+
+        expect(unvetted).to have_key(:state)
+        expect(unvetted[:state]).to eq('Colorado')
+
+        expect(unvetted).to have_key(:city)
+        expect(unvetted[:city]).to be_a(String)
+
+        expect(unvetted).to have_key(:street)
+        expect(unvetted[:street]).to be_a(String)
+
+        expect(unvetted).to have_key(:unit)
+        expect(unvetted[:unit]).to be_a(String)
+
+        expect(unvetted).to have_key(:zip)
+        expect(unvetted[:zip]).to be_a(String)
+
+        expect(unvetted).to have_key(:phone)
+        expect(unvetted[:phone]).to be_a(String)
+
+        expect(unvetted).to have_key(:cost)
+        expect(unvetted[:cost]).to be_a(String)
+
+        expect(unvetted).to have_key(:vetted)
+        expect(unvetted[:vetted]).to eq(false)
+
+        expect(unvetted).to have_key(:specialties)
+        expect(unvetted[:specialties]).to be_an(Array)
+
+        expect(unvetted).to have_key(:insurances)
+        expect(unvetted[:insurances]).to be_an(Array)
+      end
+    end
+
+    it 'returns all unvetted professionals in CO if vetted=false & state=Colorado' do 
+      create(:doctor, vetted: false, state: 'Colorado')
+      create(:mental_health_professional, vetted: false, state: 'Colorado')
+
+      doctors            = create_list(:doctor, 10, vetted: false)
+      mhps               = create_list(:mental_health_professional, 10, vetted: false)
+
+      insurances         = create_list(:insurance, 10)
+      specialties        = create_list(:specialty, 10)
+
+      doctor_insurances  = create_list(:doctor_insurance, 10, doctor: doctors.sample, insurance: insurances.sample)
+      doctor_specialties = create_list(:doctor_specialty, 10, doctor: doctors.sample, specialty: specialties.sample)
+
+      mhp_insurances  = create_list(:mhp_insurance, 10, mental_health_professional: mhps.sample, insurance: insurances.sample)
+      mhp_specialties = create_list(:mhp_specialty, 10, mental_health_professional: mhps.sample, specialty: specialties.sample)
+
+      get '/api/v1/medical_professionals', params: {vetted: false, state: 'Colorado'}
+      
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      unvetted_pros = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(unvetted_pros).to have_key(:data)
+      expect(unvetted_pros[:data].count).to eq(3)
+      expect(unvetted_pros[:data]).to be_a(Hash)
+
+      expect(unvetted_pros[:data]).to have_key(:id)
+      expect(unvetted_pros[:data][:id]).to eq(nil)
+      
+      expect(unvetted_pros[:data]).to have_key(:type)
+      expect(unvetted_pros[:data][:type]).to eq('unvetted_professionals')
+
+      expect(unvetted_pros[:data]).to have_key(:attributes)
+      expect(unvetted_pros[:data][:attributes]).to be_a(Hash)
+      expect(unvetted_pros[:data][:attributes].keys).to eq([:doctors, :mhps])
+
+      expect(unvetted_pros[:data][:attributes][:doctors]).to be_an(Array)
+      expect(unvetted_pros[:data][:attributes][:mhps]).to be_an(Array)
+      
+      expect(unvetted_pros[:data][:attributes][:doctors].count.zero?).to eq(false)
+      expect(unvetted_pros[:data][:attributes][:mhps].count.zero?).to eq(false)
+
+      unvetted_pros[:data][:attributes][:doctors].each do |unvetted|
+        expect(unvetted).to have_key(:id)
+        expect(unvetted[:id]).to be_an(Integer)
+
+        expect(unvetted).to have_key(:first_name)
+        expect(unvetted[:first_name]).to be_a(String)
+
+        expect(unvetted).to have_key(:state)
+        expect(unvetted[:state]).to eq('Colorado')
+
+        expect(unvetted).to have_key(:city)
+        expect(unvetted[:city]).to be_a(String)
+
+        expect(unvetted).to have_key(:street)
+        expect(unvetted[:street]).to be_a(String)
+
+        expect(unvetted).to have_key(:unit)
+        expect(unvetted[:unit]).to be_a(String)
+
+        expect(unvetted).to have_key(:zip)
+        expect(unvetted[:zip]).to be_a(String)
+
+        expect(unvetted).to have_key(:phone)
+        expect(unvetted[:phone]).to be_a(String)
+
+        expect(unvetted).to have_key(:vetted)
+        expect(unvetted[:vetted]).to eq(false)
+
+        expect(unvetted).to have_key(:specialties)
+        expect(unvetted[:specialties]).to be_an(Array)
+
+        expect(unvetted).to have_key(:insurances)
+        expect(unvetted[:insurances]).to be_an(Array)
+      end
+
+      unvetted_pros[:data][:attributes][:mhps].each do |unvetted|
+        expect(unvetted).to have_key(:id)
+        expect(unvetted[:id]).to be_an(Integer)
+
+        expect(unvetted).to have_key(:first_name)
+        expect(unvetted[:first_name]).to be_a(String)
+
+        expect(unvetted).to have_key(:state)
+        expect(unvetted[:state]).to eq('Colorado')
+
+        expect(unvetted).to have_key(:city)
+        expect(unvetted[:city]).to be_a(String)
+
+        expect(unvetted).to have_key(:street)
+        expect(unvetted[:street]).to be_a(String)
+
+        expect(unvetted).to have_key(:unit)
+        expect(unvetted[:unit]).to be_a(String)
+
+        expect(unvetted).to have_key(:zip)
+        expect(unvetted[:zip]).to be_a(String)
+
+        expect(unvetted).to have_key(:phone)
+        expect(unvetted[:phone]).to be_a(String)
+
+        expect(unvetted).to have_key(:cost)
+        expect(unvetted[:cost]).to be_a(String)
 
         expect(unvetted).to have_key(:vetted)
         expect(unvetted[:vetted]).to eq(false)
