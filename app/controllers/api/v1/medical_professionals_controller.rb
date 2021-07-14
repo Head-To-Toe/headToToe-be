ad6c24b86f3dc6b04ecf7a6900aebc068694cf6d
Aidan McGuire <>
2021-07-14 17:28:50 -0600

    Run rubocop

--- app/controllers/api/v1/medical_professionals_controller.rb
+++ app/controllers/api/v1/medical_professionals_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::MedicalProfessionalsController < ApplicationController
   def index
-    if !params[:vetted].present? || params[:vetted] == true
-      medical_professionals = MedicalProfessionalsFacade.get_medical_professionals({type: params[:type], state: params[:state]})
-      
+    if params[:vetted].blank? || params[:vetted] == true
+      medical_professionals = MedicalProfessionalsFacade.get_medical_professionals({ type: params[:type],
+                                                                                     state: params[:state] })
+
       case params[:type]
       when 'doctor'
         render json: DoctorSerializer.new(medical_professionals).serializable_hash
@@ -10,31 +11,32 @@ class Api::V1::MedicalProfessionalsController < ApplicationController
         render json: MhpSerializer.new(medical_professionals).serializable_hash
       end
     elsif params[:vetted] == 'false'
-      unvetted_professionals = MedicalProfessionalsFacade.get_unvetted_professionals({type: params[:type], state: params[:state]})
+      unvetted_professionals = MedicalProfessionalsFacade.get_unvetted_professionals({ type: params[:type],
+                                                                                       state: params[:state] })
       render json: UnvettedProfessionalsSerializer.new(unvetted_professionals).serializable_hash
     end
   end
 
   def create
     return render status: :unauthorized if unauthorized
-    
-    if params[:profession].present? && params[:first_name].present? && 
-      params[:last_name].present?   && params[:insurance].present?  &&
-      params[:state].present?
+
+    if params[:profession].present? && params[:first_name].present? &&
+       params[:last_name].present? && params[:insurance].present? &&
+       params[:state].present?
       case params[:profession]
       when 'doctor'
         MedicalProfessionalsFacade.create_doctor_records(
           doctor_params, params[:insurance],
           params[:specialties], params[:profession]
         )
-  
+
         render status: :created
       when 'mhp'
         MedicalProfessionalsFacade.create_mhp_records(
           mhp_params, params[:insurance],
           params[:specialties], params[:profession]
         )
-  
+
         render status: :created
       else
         render status: :unprocessable_entity
@@ -42,7 +44,6 @@ class Api::V1::MedicalProfessionalsController < ApplicationController
     else
       render status: :unprocessable_entity
     end
-
   end
 
   private
