5f9413bd695f3a13c152f2f3d0b4ec935b0ef2fd
austinandrade <austinmandrade@gmail.com>
2021-07-15 19:17:11 -0400

    Destroy dependent joint records upon parent record deletion

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,8 +4,8 @@ class Doctor < ApplicationRecord
   validates :state, presence: true
   validates :vetted, inclusion: { in: [true, false] }
 
-  has_many :doctor_insurances
-  has_many :doctor_specialties
+  has_many :doctor_insurances, dependent: :destroy
+  has_many :doctor_specialties, dependent: :destroy
   has_many :specialties, through: :doctor_specialties
   has_many :insurances, through: :doctor_insurances
 end
