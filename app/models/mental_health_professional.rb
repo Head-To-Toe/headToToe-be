719d81426a6ed955d83c1b7d11f6233dad098409
austinandrade <austinmandrade@gmail.com>
2021-07-15 19:17:11 -0400

    Destroy dependent joint records upon parent record deletion

--- app/models/mental_health_professional.rb
+++ app/models/mental_health_professional.rb
@@ -4,8 +4,8 @@ class MentalHealthProfessional < ApplicationRecord
   validates :state, presence: true
   validates :vetted, inclusion: { in: [true, false] }
 
-  has_many :mhp_insurances
-  has_many :mhp_specialties
+  has_many :mhp_insurances, dependent: :destroy
+  has_many :mhp_specialties, dependent: :destroy
   has_many :specialties, through: :mhp_specialties
   has_many :insurances, through: :mhp_insurances
 end
