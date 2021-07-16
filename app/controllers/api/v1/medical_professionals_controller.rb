c87e194ae2a9e8eaaf8918cdf9f854d7a9b0e70a
austinandrade <austinmandrade@gmail.com>
2021-07-15 23:21:22 -0400

    Refactor if conditions with guard clauses

--- app/controllers/api/v1/medical_professionals_controller.rb
+++ app/controllers/api/v1/medical_professionals_controller.rb
@@ -19,31 +19,28 @@ class Api::V1::MedicalProfessionalsController < ApplicationController
 
   def create
     return render status: :unauthorized if unauthorized
+    return render status: :unprocessable_entity unless params[:first_name] &&
+                                                       params[:last_name]  &&
+                                                       params[:profession] &&
+                                                       params[:insurance]
+    return render status: :unprocessable_entity unless params[:profession] == 'doctor' ||
+                                                       params[:profession] == 'mhp'
 
-    if params[:profession].present? && params[:first_name].present? &&
-       params[:last_name].present? && params[:insurance].present? &&
-       params[:state].present?
-      case params[:profession]
-      when 'doctor'
-        successful_creation = MedicalProfessionalsFacade.create_doctor_records(
-          doctor_params, params[:insurance],
-          params[:specialties], params[:profession]
-        )
+    case params[:profession]
+    when 'doctor'
+      successful_creation = MedicalProfessionalsFacade.create_doctor_records(
+        doctor_params, params[:insurance],
+        params[:specialties], params[:profession]
+      )
 
-        successful_creation ? (render status: :created) : (render status: :conflict)
+      successful_creation ? (render status: :created) : (render status: :conflict)
 
-      when 'mhp'
-        successful_creation = MedicalProfessionalsFacade.create_mhp_records(
-          mhp_params, params[:insurance],
-          params[:specialties], params[:profession]
-        )
-        successful_creation ? (render status: :created) : (render status: :conflict)
-
-      else
-        render status: :unprocessable_entity
-      end
-    else
-      render status: :unprocessable_entity
+    when 'mhp'
+      successful_creation = MedicalProfessionalsFacade.create_mhp_records(
+        mhp_params, params[:insurance],
+        params[:specialties], params[:profession]
+      )
+      successful_creation ? (render status: :created) : (render status: :conflict)
     end
   end
 
