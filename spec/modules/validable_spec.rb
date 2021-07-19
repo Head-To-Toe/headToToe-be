a5e6d35088594a4385bf57d42bc26fb38b476aa6
Aidan McGuire <aidanmcguire211@gmail.com>
2021-07-19 17:57:05 -0600

    [Validable] Add tests for check_type

--- spec/modules/validable_spec.rb
+++ spec/modules/validable_spec.rb
@@ -22,5 +22,26 @@ RSpec.describe 'Validable' do
       expect(check_state(Array)).to eq(false)
     end
   end
+
+  context 'check_type(type)' do
+    it 'Sets default value if none is provided' do
+      expect(check_type(nil)).to eq('all')
+    end
+
+    it 'returns true if type is valid' do
+      expect(check_type('doctor')).to eq(true)
+      expect(check_type('mhp')).to eq(true)
+    end
+
+    it 'returns false if an invalid type is passed' do
+      expect(check_type('Pudding')).to eq(false)
+    end
+
+    it 'returns false if it is not a string' do
+      expect(check_type(0)).to eq(false)
+      expect(check_type(12)).to eq(false)
+      expect(check_type(Array)).to eq(false)
+    end
+  end
 end
 
