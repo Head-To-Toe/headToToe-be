fd2c748012aed1758afee90480db216d4e854068
Aidan McGuire <aidanmcguire211@gmail.com>
2021-07-19 18:30:56 -0600

    [Validable] add set_defaults

--- app/modules/validable.rb
+++ app/modules/validable.rb
@@ -1,30 +1,32 @@
 module Validable
   def valid_params?(params)
+    set_defaults(params)
     return true if check_state(params[:state]) &&
                     check_type(params[:type]) &&
                     check_vetted(params[:vetted])
     false
   end
+
+  def set_defaults(params)
+    params[:state] = 'all' if params[:state].nil?
+    params[:type] = 'all' if params[:type].nil?
+    params[:vetted] = 'true' if params[:vetted].nil?
+  end
   
   def check_state(state)
-    return state = 'all' if state.nil?
+    return true if state == 'all'
     return false if state.class != String
     states = File.read('app/support/states.rb')
     states.include?(state)
   end
   
   def check_type(type)
-    return type = 'all' if type.nil?
+    return true if type == 'all'
     return false if type.class != String
-    type == 'doctor' || type == 'mhp'
+    type == 'doctor' || type == 'mhp' || type == 'all'
   end
   
   def check_vetted(vetted)
-    vetted = 'true' if vetted.nil?
     ['true', 'false'].include?(vetted)
   end
 end
-
-# vetted
-# state
-# type
