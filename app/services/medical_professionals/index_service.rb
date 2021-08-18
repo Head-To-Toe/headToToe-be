96d88e04817abac7d64d1d38665f266b1a4c1c8c
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-18 15:56:28 -0600

    Refactor format_mhps and format_doctors to not return insurance and specialty time stamps

--- app/services/medical_professionals/index_service.rb
+++ app/services/medical_professionals/index_service.rb
@@ -33,8 +33,8 @@ class MedicalProfessionals::IndexService
           zip: doctor.zip,
           phone: doctor.phone,
           vetted: doctor.vetted,
-          specialties: doctor.specialties,
-          insurances: doctor.insurances
+          specialties: doctor.specialties.map { |specialty| specialty.name },
+          insurances: doctor.insurances.map { |insurance| insurance.company }
         }
       end
     end
@@ -53,8 +53,8 @@ class MedicalProfessionals::IndexService
           phone: mhp.phone,
           vetted: mhp.vetted,
           cost: mhp.cost,
-          specialties: mhp.specialties,
-          insurances: mhp.insurances
+          specialties: mhp.specialties.map { |specialty| specialty.name },
+          insurances: mhp.insurances.map { |insurance| insurance.company }
         }
       end
     end
