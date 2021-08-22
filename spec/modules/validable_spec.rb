7ecebc310d81139b13ac220206829b098c6dedd5
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-22 11:17:19 -0600

    Refactor valid_update_or_destroy(params) to account for string id params

--- spec/modules/validable_spec.rb
+++ spec/modules/validable_spec.rb
@@ -165,20 +165,20 @@ RSpec.describe 'Validable' do
 
   context 'valid_update_or_destroy?(params)' do
     it 'returns true if all required params are included' do
-      params1 = { id: 1, profession: 'doctor'}
-      params2 = { id: 2, profession: 'mhp'}
+      params1 = { id: '1', profession: 'doctor'}
+      params2 = { id: '2', profession: 'mhp'}
 
       expect(valid_update_or_destroy?(params1)).to eq(true)
       expect(valid_update_or_destroy?(params2)).to eq(true)
     end
 
     it 'returns false if profession is not doctor or mhp' do
-      params1 = { id: 1, profession: 'fancy'}
-      params2 = { id: 2, profession: ''}
-      params3 = { id: 2, profession: 1}
-      params4 = { id: 2, profession: nil}
-      params5 = { id: 2, profession: ['doctor']}
-      params6 = { id: 2}
+      params1 = { id: '1', profession: 'fancy'}
+      params2 = { id: '2', profession: ''}
+      params3 = { id: '2', profession: 1}
+      params4 = { id: '2', profession: nil}
+      params5 = { id: '2', profession: ['doctor']}
+      params6 = { id: '2'}
 
       expect(valid_update_or_destroy?(params1)).to eq(false)
       expect(valid_update_or_destroy?(params2)).to eq(false)
@@ -188,7 +188,7 @@ RSpec.describe 'Validable' do
       expect(valid_update_or_destroy?(params6)).to eq(false)
     end
 
-    it 'returns false if id is not an integer' do
+    it 'returns false if id is not a valid id' do
       params1 = { id: 'one', profession: 'doctor'}
       params2 = { id: [1], profession: 'doctor'}
       params3 = { id: '', profession: 'doctor'}
